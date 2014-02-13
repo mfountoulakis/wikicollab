c003f0d1aa87d967810f82f6c0c96ce112ef6a2b
Manos <fountoulakismanos@gmail.com>
2014-02-12 23:11:29 -0500

    initialized app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wikicollab::Application.config.secret_key_base = '1aadd1e9fe804f98626d5c180a5bb1aa514939c864fc2b3bb8f67cfd5a01219531ae37e410498e8c89aa8a3b5533718c91ea39df609e2004f5206ac8a092dac9'
