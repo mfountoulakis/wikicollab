d7db123b7b79f7cc1f5dd552f1797c1c4a35849e
Manos <fountoulakismanos@gmail.com>
2014-03-04 15:05:06 -0500

    added full sign_up_spec

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -20,8 +20,8 @@ feature 'Visitor signs in' do
 
   def sign_in_with(email, password)
     visit new_user_session_path
-    fill_in 'Email', with: 
-    fill_in 'Password', with: 
+    fill_in 'Email', with: email
+    fill_in 'Password', with: password
     click_button 'Sign in'
   end
 end
