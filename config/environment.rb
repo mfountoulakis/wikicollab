d6ca61b7ca451da895f1a2546601a2a2f51053d5
Manos <fountoulakismanos@gmail.com>
2014-03-20 02:59:47 -0400

    config actionmailer

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,6 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-config.action_mailer.default_url_options = { :host => 'dry-taiga-5167.herokuapp.com' }  
-
 
 # Initialize the Rails application.
 Wikicollab::Application.initialize!
